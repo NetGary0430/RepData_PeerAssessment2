c0c7df8149d5aacecdfee582119e047bcbb57792
NetGary0430 <garynetherton@gmail.com>
2015-08-20 11:02:14 -0700

    Code & Markdown updates

--- repData_PeerAssessment2.R
+++ repData_PeerAssessment2.R
@@ -1,6 +1,34 @@
 ## R Script for Peer Assessment #2 - Reproducible Research
 ##
 
-## Read in NoAA data
+## Load the appropriate libraries
+library("dplyr")
+lirbrary("ggplot2")
 
+## Read in NoAA data - note that read.csv can read directly from compressed files
 noaaDB <- read.csv("repdata-data-StormData.csv.bz2")
+
+## For dplyr, tbl_df is easier to use
+noaaDB <- tbl_df(noaaDB)
+
+glimpse(noaaDB)
+
+## Rank Event Types by cost to human health
+dfDamage <- noaaDB %>%
+    group_by(EVTYPE) %>%
+    summarise(p_Fatal = sum(FATALITIES, na.rm = TRUE), p_Injured = sum(INJURIES, na.rm = TRUE)) %>%
+    mutate(rank = rank(p_Fatal)) %>%
+    arrange(desc(rank), desc(p_Injured))
+
+## Rank Event Types by cost to economic health
+dfDamageEcon <- noaaDB %>%
+    group_by(EVTYPE) %>%
+    summarise(p_PropDmg = sum(PROPDMG, na.rm = TRUE), p_CropDmg = sum(CROPDMG, na.rm = TRUE)) %>%
+    mutate(rank = rank(p_PropDmg)) %>%
+    arrange(desc(rank), desc(p_CropDmg))
+
+totImpace <- full_join(dfDamage, dfDamageEcon, by = "EVTYPE")
+
+
+
+
